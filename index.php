7eb80989dd667024b0745f82009e2a6f7db19707
Rieg <47364961+LeoRieg@users.noreply.github.com>
2019-06-27 21:40:17 -0300

    comit
    net podre não funciona
    close #6 
    close #3 
    close #4 
    close #1 
    close #5

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <title>Sistema de Login</title>
+        <style>
+            #alerta,#caixaRegistro,#caixaSenha{
+                display: none;
+            }
+        </style>
+
     </head>
     <body class="bg-dark">
         <main class="container mt-4">
@@ -78,12 +84,101 @@
 
             </section>
             <br>
+
+            <section class="row">
+
+                <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
+                    <h2 class="text-center mt-2">
+                        Registrar-se 
+                    </h2>
+                    <form action="#" method="post" role="form" class="p-2"
+                          id="formRegistro">
+
+                        <div class="form-group">
+                            <input type="text" name="nomeCompleto"
+                                   class="form-control"
+                                   placeholder="Nome completo" required 
+                                   minlength="6">
+                        </div>
+                        <div class="form-group">
+                            <input type="text" name="nomeUsuario"
+                                   class="form-control"
+                                   placeholder="Nome usuário" required 
+                                   minlength="6">
+
+                        </div>
+                        <!-- campo email-->
+                        <div class="form-group">
+                            <input type="email" name="emailUsuario"
+                                   class="form-control"
+                                   placeholder="E-mail" required>
+
+                        </div>
+
+                        <!-- campo senha-->
+                        <div class="form-group">
+                            <input type="password" id="senhaUsuario"
+                                   class="form-control"
+                                   placeholder="Senha" required 
+                                   minlength="6">
+
+                        </div>
+                        <!-- campo de configuração de senha -->
+                        <div class="form-group">
+                            <input type="password"
+                                   id="senhaUsuarioConfimar"
+                                   class="form-control"
+                                   placeholder="Confirmar a senha" required 
+                                   minlength="6">    
+                        </div>
+                        <!-- check box de aceite -->
+
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" name="concordar"
+                                       class="custom-control-input"
+                                       id="checkConcordar">
+
+                                <label for="CheckConcordar"
+                                       class="custom-control-label">
+                                    Eu concordo com os
+                                    <a href="#">
+                                        termos e condições s2.
+                                    </a>
+
+                                </label>
+                            </div>
+                        </div>
+
+                        <!-- botao enviar -->
+
+                        <div class="form-group"> 
+                            <input type="submit"
+                                   name="btnRegistroUsuario"
+                                   id="btnRegistroUsuario"
+                                   value="Registrar"
+                                   class="btn btn-primary btn-block">
+                        </div>
+
+                        <div class="form-group">
+                            <p class="text-center">
+                                Já registrado?
+                                <a href="#" id="btnEntrarRegistrado">Entrar aqui.</a>
+                            </p>        
+                        </div>
+
+                    </form>
+                </div>
+            </section>
+
+
+
             <!-- formulário de recuperação de senha-->
             <section class="row">
                 <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaSenha">
                     <h2 class="text-center mt-2">
                         Gerar nova senha
-                    </h2>
+                    </h2> 
                     <form action="#" method="post" role="form"
                           class="p-2" id="formSenha">
                         <div class="form-group">
@@ -103,16 +198,16 @@
                         </div>
                         <div class="form-group float-right">
                             <a href="#" id="btnVoltar">
-                               Voltar
+                                Voltar
                             </a>
                         </div>
-                       
+
                     </form>
                 </div>
             </section>
-            
+
             <!-- formulário de recuperação de senha-->
-            
+
         </main>
 
 
@@ -122,5 +217,31 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script>
+            //$(document).ready(function(){})
+            //JQuery
+            $(function(){
+                $("#btnEsqueci").click(function(){
+                    $("#caixaLogin").hide();
+                    $("#caixaSenha").show();
+                });
+                //volta para o login
+                $("#btnVoltar").click(function(){
+                    $("#caixaLogin").show();
+                    $("#caixaSenha").hide();
+                });
+                //registrar no login
+                $("#btnRegistrar").click(function(){
+                    $("#caixaLogin").hide();
+                    $("#caixaRegistro").show();
+                });
+                //login no registro
+                 $("#btnEntrarRegistrado").click(function(){
+                    $("#caixaLogin").show();
+                    $("#caixaRegistro").hide();
+                });
+                
+            });
+        </script>
     </body>
 </html>
